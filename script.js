c5efc77c2edde52ff33e9c651f8f280eb28caf28
Natthanicha VONGJARIT <161776125+callanicha@users.noreply.github.com>
2024-09-14 17:43:27 +0200

    update 14 sept

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("toggle-btn")
+const navBar = document.querySelector(".navBar")
 
 btn.addEventListener("click", () => {
-	console.log("you clicked the btn !!")
 	document.querySelector(".navBar").classList.toggle("show")
 })
